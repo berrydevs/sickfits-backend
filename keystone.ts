6f379703c940dcdad5e27db554ad481cf2a6cd42
Tamir Azrab <tamirazrab@gmail.com>
2021-12-31 00:18:53 +0500

    feat: :tada: Initial commit with some schemas of Keystone

---
+++ keystone.ts
@@ -0,0 +1,47 @@
+import "dotenv/config"
+import { config, createSchema } from '@keystone-next/keystone/schema'
+import { createAuth } from "@keystone-next/auth"
+import { User } from './schemas/User';
+import { withItemData, statelessSessions } from "@keystone-next/keystone/session"
+import { Product } from './schemas/Product';
+import { ProductImage } from "./schemas/ProductImage";
+
+const databaseURL = process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fit"
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password',]
+  }
+})
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET
+}
+
+export default withAuth(config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+
+  },
+  lists: createSchema({
+    // TODO Schema goes here
+    User, Product, ProductImage
+  }),
+  ui: {
+    isAccessAllowed: ({ session }) => {
+      return !!session?.data
+    }
+  },
+  session: withItemData(statelessSessions(sessionConfig), { User: 'id' })
+}))
